2cd7d6ac08f6c4c6ac5ec834558afd668bbb1a8f
Diederik Veeze <d.veeze@zestsoftware.nl>
2015-08-07 12:58:50 +0200

    Fix description

--- collective/plonetruegallery/portlets.py
+++ collective/plonetruegallery/portlets.py
@@ -48,8 +48,8 @@ class IGalleryPortlet(IPortletDataProvider):
     width = schema.Int(
         title=_(u"gallery_portlet_width_title", default=u"Width in pixels"),
         description=_(u"gallery_portlet_width_description",
-            default=u"The width of the image in the portlet"
-                u"If 0, no height is set."),
+            default=u"The width of the image in the portlet. "
+                u"If 0, no width is set."),
         required=True,
         default=0)
 
